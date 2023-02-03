7a1abe0bd9d14fa30ec21a9e898abd2dab1460cd
K K Haridev <67409998+KKHaridev@users.noreply.github.com>
2023-02-03 14:05:19 +0530

    Removed line numbers

--- PLSQL/Expt_10_stud.sql
+++ PLSQL/Expt_10_stud.sql
@@ -1,49 +1,43 @@
-1 SET SERVEROUTPUT ON
-2
-3 select user from dual;
-4 SELECT NAME FROM v$database;
-5 select table_name from dba_tables;
-6 select owner, table_name from all_tables;
-7 select table_name FROM user_tables;
-8 SELECT table_name FROM user_tables ORDER BY table_name;
-9
-10 create table Stud(rollno int primary key,
-11 name char(10),
-12 mark1 float,
-13 mark2 float,
-14 mark3 float);
-15
-16 /* ROLLNO NAME MARK1 MARK2 MARK3
-17 ---------- ---------- --------- ---------- ----------
-18 1 aparna 80 90 78
-19 2 amritha 90 92 81
-20 3 binuja 23 18 20
-21 4 cathy 49 50 50
-22 5 danish 60 62 61
-23 6 fayas 76 62 74 */
-24
-25 insert into Stud values(1,'aparna', 80, 90, 78);
-26 insert into Stud values(&rollno, '&name', &mark1, &mark2, &mark3);
-27 select * from Stud;
-28 select name from Stud where rollno=1;
-29
-30
+ SET SERVEROUTPUT ON
+
+ select user from dual;
+ SELECT NAME FROM v$database;
+ select table_name from dba_tables;
+ select owner, table_name from all_tables;
+ select table_name FROM user_tables;
+ SELECT table_name FROM user_tables ORDER BY table_name;
+
+ create table Stud(rollno int primary key,
+ name char(10),
+ mark1 float,
+ mark2 float,
+ mark3 float);
+
+ /* ROLLNO NAME MARK1 MARK2 MARK3
+ ---------- ---------- --------- ---------- ----------
+ 1 aparna 80 90 78
+ 2 amritha 90 92 81
+ 3 binuja 23 18 20
+ 4 cathy 49 50 50
+ 5 danish 60 62 61
+ 6 fayas 76 62 74 */
+
+ insert into Stud values(1,'aparna', 80, 90, 78);
+ insert into Stud values(&rollno, '&name', &mark1, &mark2, &mark3);
+ select * from Stud;
+ select name from Stud where rollno=1;
 
 
+ DECLARE
+ age integer;
+ name VARCHAR(20);
+ BEGIN
+ dbms_output.put_line('Hello world');
+ --dbms_output.put_line('age = ' || age);
+ --dbms_output.put_line('name = ' || name);
+ --insert into Stud values(&rollno, '&name', &mark1, &mark2, &mark3);
+ END;
 
-31 DECLARE
-32 age integer;
-33 name VARCHAR(20);
-34 BEGIN
-35 dbms_output.put_line('Hello world');
-36 --dbms_output.put_line('age = ' || age);
-37 --dbms_output.put_line('name = ' || name);
-38 --insert into Stud values(&rollno, '&name', &mark1, &mark2, &mark3);
-39 END;
-
-
-
-40
-41 select * from Stud;
-42 delete from Stud;
-43 drop table Stud purge;
+ select * from Stud;
+ delete from Stud;
+ drop table Stud purge;
