e7984654f98a7022f1409fec095e82ea1f418342
K K Haridev <67409998+KKHaridev@users.noreply.github.com>
2023-02-03 14:07:14 +0530

    Update additionplsql.sql

--- PLSQL/additionplsql.sql
+++ PLSQL/additionplsql.sql
@@ -1,9 +1,9 @@
-45 /* addition of two numbers */
-46 DECLARE
-47 a integer := &a;
-48 b integer := &b;
-49 res int;
-50 BEGIN
-51 res := a + b;
-52 dbms_output.put_line('result = ' || res);
-53 END;
+/* addition of two numbers */
+ DECLARE
+ a integer := &a;
+ b integer := &b;
+ res int;
+ BEGIN
+ res := a + b;
+ dbms_output.put_line('result = ' || res);
+ END;
